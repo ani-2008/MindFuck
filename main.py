985ce9774031509a591ef8b63c84a4422dd4c0b8
Anirudh <95073991+ani-2008@users.noreply.github.com>
2025-05-03 18:22:36 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,91 @@
+import sys
+def read_file():
+    if len(sys.argv) == 2 and sys.argv[1].endswith(".mf"):
+        with open(sys.argv[1]) as file:
+            data = file.read().replace(" ","").replace("\n","")
+        
+        return data
+
+code_pointer = 0
+memory_stack = [0] * 30000
+memory_pointer = 0
+code = read_file()
+
+def code_exec(memory_stack,code_pointer,code,memory_pointer):
+    while code_pointer < len(code):
+        
+        if code[code_pointer] == "+":
+            if memory_stack[memory_pointer] < 255:
+                memory_stack[memory_pointer] += 1
+            
+        elif code[code_pointer] == "-":
+            if memory_stack[memory_pointer] > 0:
+                memory_stack[memory_pointer] -= 1
+
+        elif code[code_pointer] == "*":
+            result = memory_stack[memory_pointer] * memory_stack[memory_pointer]
+            if result <= 255:
+                memory_stack[memory_pointer] = result
+
+        elif code[code_pointer] == "/":
+            if code[code_pointer] != 0:
+                result = int(code[code_pointer-1]/code[code_pointer])
+                if result < 255:
+                    memory_stack[memory_pointer] = result
+
+        elif code[code_pointer] == "<":
+            if memory_pointer > 0:
+                memory_pointer -= 1
+            
+        elif code[code_pointer] == ">":
+            if memory_pointer < 30000:
+                memory_pointer += 1
+
+        elif code[code_pointer] == "$":
+            get_in = input()
+            if len(get_in) == 1 and ord(get_in) <= 255:
+                memory_stack[memory_pointer] = get_in
+
+        elif code[code_pointer] == ":":
+            print(memory_stack[memory_pointer])
+        
+        elif code[code_pointer] == "L":
+            if memory_pointer != 0:
+                if memory_stack[memory_pointer - 1] < memory_stack[memory_pointer]:
+                    memory_stack[memory_pointer] = 1
+                else:
+                    memory_stack[memory_pointer] = 0
+            
+        elif code[code_pointer] == "G":
+            if memory_pointer != 0 :
+                if memory_stack[memory_pointer - 1] > memory_stack[memory_pointer]:
+                    memory_stack[memory_pointer] = 1
+                else:
+                    memory_stack[memory_pointer] = 0
+
+        elif code[code_pointer] == "(":
+            if memory_stack[memory_pointer] == 0:
+                count = 1
+                while count > 0:
+                    code_pointer += 1
+                    if code[code_pointer] == "(":
+                        count += 1
+                    elif code[code_pointer] == ")":
+                        count -= 1
+
+        elif code[code_pointer] == ")":
+            if memory_stack[memory_pointer] != 0:
+                count = 1
+                while count > 0:
+                    code_pointer -= 1
+                    if code[code_pointer] == ")":
+                        count += 1
+                    elif code[code_pointer] == "(":
+                        count -= 1
+        else:
+            pass
+        
+        code_pointer += 1
+
+
+code_exec(memory_stack,code_pointer,code,memory_pointer)
